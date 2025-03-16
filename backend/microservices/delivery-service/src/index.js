25f93fe8cc62d20312edd13815c3d3fad64b1844
Hasindu Koshitha <koshithahasindu@gmail.com>
2025-03-16 17:34:49 +0530

    Added dockerfile

--- backend/microservices/delivery-service/src/index.js
+++ backend/microservices/delivery-service/src/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -11,18 +11,19 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Test Route
-app.get('/', (req, res) => {
-  res.send('Welcome to FlavorFleet Backend!');
+app.get("/", (req, res) => {
+  res.send("Welcome to FlavorFleet Backend!");
 });
 
 // New API Endpoint
-app.get('/api/delivery', (req, res) => {
-  res.json({ message: 'Hello from the backend delivery-service!' });
+app.get("/api/delivery", (req, res) => {
+  res.json({ message: "Hello from the backend delivery-service!" });
 });
 
 // Start Server

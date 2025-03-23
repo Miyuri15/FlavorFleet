7005b38ac4a2c5f7abc0ec353ac70c9a2b3fc1fd
Hasindu Koshitha <koshithahasindu@gmail.com>
2025-03-23 17:14:19 +0530

    Refactor auth routes

--- backend/gateway/src/index.js
+++ backend/gateway/src/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5004;
@@ -11,28 +11,28 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-  const authRoutes = require("./routes/userRoutes");
-  app.use("/api/auth", authRoutes);
+const authRoutes = require("./routes/userRoutes");
+app.use("/api/auth", authRoutes);
 
-  // Error handling middleware
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-
-  // Test Route
-app.get('/', (req, res) => {
-  res.send('Welcome to FlavorFleet Backend!');
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Welcome to FlavorFleet Backend!");
 });
 
 // New API Endpoint
-app.get('/api/message', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from the backend!" });
 });
 
 // Start Server
